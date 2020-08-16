1c295ae2eb0bf9500bda5f027f4ba23687edb1da
Nicohuertasl <nicolashuertaslara@hotmail.com>
2020-08-15 20:41:06 -0500

    Definicion de estilos en css puro

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+//
 window.onload = function(){
-  // alert("Hello world");
+var display = document.getElementById('display');
+var button_division = document.getElementById('button_division');
+var button_multiplicacion = document.getElementById('button_multiplicacion');
+var button_suma = document.getElementById('button_suma');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+var  = document.getElementById('');
+
+
+
 }
